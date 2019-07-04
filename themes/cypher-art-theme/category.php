b339ac46e4725e320255319859f2ff914cac2b01
Ivan Podkidyshev <raashicat@gmail.com>
2019-07-04 14:23:17 +0400

    Completed category template, fixed querying function to not include posts in children categories

--- themes/cypher-art-theme/category.php
+++ themes/cypher-art-theme/category.php
@@ -1,3 +1,51 @@
 <?php get_header() ?>
-<p>In development</p>
+<?php
+function ca_write_posts($cat_ID) {
+	$args = array(
+		'posts_per_page' => -1,
+		'post_type' => 'post',
+		'category__in' => array($cat_ID)
+	);
+	$query = new WP_Query($args);
+	if ($query->have_posts()) {
+		while ($query->have_posts()) {
+			$query->the_post();
+			?>
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><hr>
+			<?php
+		}
+		wp_reset_postdata();
+	}
+}
+?>
+<div id="ca-main">
+    <div id="ca-cyphers" class="ca-block">
+        <?php
+        $category = get_queried_object();
+        $root_category_id = $category->parent;
+        if ($root_category_id == null)
+	        // поддержка постов внутри корневой рубрики
+	        $root_category_id = $category->cat_ID;
+        $root_category = get_category($root_category_id);
+
+        $args = array('parent' => $root_category_id);
+        $children_categories = get_categories($args);
+
+        // посты в корневой рубрике
+        ca_write_posts($root_category_id);
+        // посты в подрубриках
+        foreach ($children_categories as $cat) { ?>
+            <div class="ca-cyphers-cat">
+                <a href="<?php echo get_category_link($cat->cat_ID) ?>"><b><?php echo $cat->cat_name; ?></b></a>
+                <br>
+		        <?php ca_write_posts($cat->cat_ID); ?>
+            </div>
+        <?php } ?>
+    </div>
+
+    <div id="ca-content" class="ca-block">
+        <h2><?php single_cat_title(); ?></h2>
+	    <?php echo category_description(); ?>
+    </div>
+</div>
 <?php get_footer() ?>
--- themes/cypher-art-theme/single.php
+++ themes/cypher-art-theme/single.php
@@ -1,27 +1,28 @@
-<?php get_header(); ?> 
+<?php get_header(); ?>
+<?php
+function ca_write_posts($cat_ID) {
+	$args = array(
+		'posts_per_page' => -1,
+		'post_type' => 'post',
+		'category__in' => array($cat_ID)
+	);
+	$query = new WP_Query($args);
+	if ($query->have_posts()) {
+		while ($query->have_posts()) {
+			$query->the_post();
+			?>
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><hr>
+			<?php
+		}
+		wp_reset_postdata();
+	}
+}
+?>
 
 <div id="ca-main">
 
 	<div id="ca-cyphers" class="ca-block">
 	<?php
-    function ca_write_posts($cat_ID) {
-	    $args = array(
-		    'posts_per_page' => -1,
-		    'post_type' => 'post',
-		    'cat' => $cat_ID
-	    );
-	    $query = new WP_Query($args);
-	    if ($query->have_posts()) {
-		    while ($query->have_posts()) {
-			    $query->the_post();
-			?>
-        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><hr>
-	    <?php
-	        }
-	        wp_reset_postdata();
-	    }
-    }
-
     $category = get_the_category()[0];
     $root_category_id = $category->parent;
     if ($root_category_id == null)
