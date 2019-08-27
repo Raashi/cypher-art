eb7cf670692a57e1915cb2d7e523a6f8239b8983
Ivan Podkidyshev <raashicat@gmail.com>
2019-08-27 22:53:42 +0400

    Improved cats block appearance

--- themes/wen-theme/cats.php
+++ themes/wen-theme/cats.php
@@ -1,5 +1,5 @@
 <?php
-function wen_write_cats($cat_ID, $cat_html_class) {
+function wen_write_posts_titles($cat_ID, $cat_html_class) {
 	$args = array(
 		'posts_per_page' => -1,
 		'post_type' => 'post',
@@ -10,11 +10,11 @@ function wen_write_cats($cat_ID, $cat_html_class) {
 		while ($query->have_posts()) {
 			$query->the_post();
 			?>
-			<a class="<?php echo $cat_html_class ?>"
+			<a class="wen-cats-el wen-cats-post <?php echo $cat_html_class ?>"
 			   href="<?php the_permalink(); ?>">
 				<?php the_title(); ?>
-			</a>
-			<hr class="<?php echo $cat_html_class ?>">
+			</a><br>
+<!--			<hr class="--><?php //echo $cat_html_class ?><!--">-->
 			<?php
 		}
 		wp_reset_postdata();
@@ -27,22 +27,23 @@ function wen_write_cats_block($category) {
 	if ($root_category_id == null)
 		// поддержка постов внутри корневой рубрики
 		$root_category_id = $category->cat_ID;
-	$root_category = get_category($root_category_id);
 
 	$args = array('parent' => $root_category_id);
 	$children_categories = get_categories($args);
 
 	// посты в корневой рубрике
-	wen_write_cats($root_category_id, "wen-cats-root");
+    ?> <div class="wen-cats-subcat"> <?php
+	wen_write_posts_titles($root_category_id, "wen-cats-root");
+    ?> </div> <?php
 	// посты в подрубриках
 	foreach ($children_categories as $cat) { ?>
-		<div class="wen-cats-cat">
-			<a class="wen-cats-root"
+		<div class="wen-cats-subcat">
+			<a class="wen-cats-el wen-cats-cat wen-cats-root"
 			   href="<?php echo get_category_link($cat->cat_ID) ?>">
 				<b><?php echo $cat->cat_name; ?></b>
 			</a>
 			<br>
-			<?php wen_write_cats($cat->cat_ID, "wen-cats-child"); ?>
+			<?php wen_write_posts_titles($cat->cat_ID, "wen-cats-child"); ?>
 		</div>
 	<?php }
 	?> </div> <?php
