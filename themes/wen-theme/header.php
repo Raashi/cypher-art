6b58c896279929d3232963ebbf3ebde78b6e54bb
Ivan Podkidyshev <raashicat@gmail.com>
2020-03-01 12:47:29 +0400

    increased site apadtivity

--- themes/wen-theme/header.php
+++ themes/wen-theme/header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php wp_head(); ?>
 	<title><?php echo get_bloginfo(); ?></title>
 </head>
